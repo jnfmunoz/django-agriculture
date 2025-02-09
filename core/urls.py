69367441bbdd64f10ff8a664a0e8eb81eb15597f
jnfmunoz <juanmunozgrau@gmail.com>
2025-02-09 07:45:42 -0300

    3

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import HomePageView
 
 urlpatterns = [
+    path('', HomePageView.as_view(), name="index"),
+
     path('admin/', admin.site.urls),
 ]
---
+++ core/views.py
@@ -0,0 +1,9 @@
+from django.views.generic.base import TemplateView
+from django.shortcuts import render
+
+# Create your views here.
+class HomePageView(TemplateView):
+    template_name = "index.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
