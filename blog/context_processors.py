0e25ea7fb83b3a2c5235eff18c29deb28982b76d
jnfmunoz <juanmunozgrau@gmail.com>
2025-03-02 17:16:44 -0300

    27

---
+++ blog/context_processors.py
@@ -0,0 +1,9 @@
+from .models import Post
+
+def user_posts(request):
+    if request.user.is_authenticated:
+        posts = Post.objects.filter(author=request.user).order_by("-created_at")  # Posts del usuario logueado
+    else:
+        posts = None  # No hay usuario autenticado
+
+    return {"user_posts": posts}
--- blog/models.py
+++ blog/models.py
@@ -8,12 +8,13 @@ class Post(models.Model):
     
     title = models.CharField(max_length=200, verbose_name='Title')
     subtitle = models.CharField(max_length=200, verbose_name='Subtitle')
+    relevant_text = models.CharField(max_length=50, verbose_name='Relevant Text', null=True, blank=True)
     content = models.TextField(verbose_name='Content')
     image = models.ImageField(verbose_name='Image', upload_to='posts', null=False, blank=False)
     author = models.ForeignKey(User, verbose_name='Author', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Creation Date")
-    updated_at = models.DateTimeField(auto_now=True, verbose_name = "Edition Date")
-    published_at = models.DateTimeField(verbose_name="Published Date", default=now)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Creation Date')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name = 'Edition Date')
+    published_at =  models.DateTimeField(verbose_name='Published Date', default=now)
 
     class Meta:
         verbose_name = "Post"
@@ -29,11 +30,12 @@ class Post(models.Model):
         if not paragraphs:  
             return ""  # Si no hay contenido, retorna vacío
         
-        first_two = "".join(f"<p>{p}</p>" for p in paragraphs[:2])  # Primeros 2 párrafos
-        blockquote = f"<blockquote><p>{self.subtitle}</p></blockquote>"  # Blockquote con el subtítulo
+        first_two = "".join(f"<p>{p}</p>" for p in paragraphs[:3])  # Primeros 2 párrafos
+        blockquote = f"<blockquote><p>{self.relevant_text}</p></blockquote>" if self.relevant_text else ""
+        subtitle = f'<h3 style="text-align:left">{ self.subtitle }</h3>' 
         remaining = "".join(f"<p>{p}</p>" for p in paragraphs[2:])  # Resto de los párrafos
 
-        return first_two + blockquote + remaining  # Concatenar todo
+        return first_two + blockquote + subtitle + remaining  # Concatenar todo
     
     def count_paragraphs(self):
         return len(re.split(r'\n+', self.content.strip()))
