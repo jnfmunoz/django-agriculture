bfb620ca1a25d9a4b84342525ef78c841ea3670d
jnfmunoz <juanmunozgrau@gmail.com>
2025-04-13 07:39:04 -0400

    32

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -2,7 +2,7 @@ from .models import Post
 
 def user_posts(request):
     if request.user.is_authenticated:
-        posts = Post.objects.filter(author=request.user).order_by("-created_at")  # Posts del usuario logueado
+        posts = Post.objects.filter(author=request.user).order_by("-created_at", "-id")  # Posts del usuario logueado
     else:
         posts = None  # No hay usuario autenticado
 
