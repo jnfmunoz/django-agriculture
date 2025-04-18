288684cd920de849e1eda8a873d11ee450997cad
jnfmunoz <juanmunozgrau@gmail.com>
2025-04-17 23:14:32 -0400

    37

--- blog/forms.py
+++ blog/forms.py
@@ -5,12 +5,14 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ['title', 'subtitle', 'relevant_text','content', 'image']
+        fields = ['title', 'subtitle', 'relevant_text','introduction', 'body_text', 'conclusion','image']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control mt-3', 'placeholder': 'Title'}),
             'subtitle': forms.TextInput(attrs={'class': 'form-control mt-3', 'placeholder': 'Subtitle'}),
             'relevant_text': forms.Textarea(attrs={'class': 'form-control mt-3', 'rows': 2, 'placeholder': 'Relevant Text'}),
-            'content': forms.Textarea(attrs={'class': 'form-control mt-3', 'rows': 6, 'placeholder': 'Post Content'}),
+            'introduction': forms.Textarea(attrs={'class': 'form-control mt-3', 'rows': 6, 'placeholder': 'Post Introduction'}),
+            'body_text': forms.Textarea(attrs={'class': 'form-control mt-3', 'rows': 6, 'placeholder': 'Body Text'}),
+            'conclusion': forms.Textarea(attrs={'class': 'form-control mt-3', 'rows': 6, 'placeholder': 'Conclusion'}),
             'image': forms.ClearableFileInput(attrs={'class': 'form-control-file mt-3'}),            
 
         }
--- blog/models.py
+++ blog/models.py
@@ -9,12 +9,13 @@ class Post(models.Model):
     title = models.CharField(max_length=200, verbose_name='Title')
     subtitle = models.CharField(max_length=200, verbose_name='Subtitle')
     relevant_text = models.CharField(max_length=50, verbose_name='Relevant Text', null=True, blank=True)
-    content = models.TextField(verbose_name='Content')
+    introduction = models.TextField(verbose_name='Introduction')
+    body_text = models.TextField(verbose_name='Body Text')
+    conclusion = models.TextField(verbose_name='Conclusion')
     image = models.ImageField(verbose_name='Image', upload_to='posts', null=False, blank=False)
     author = models.ForeignKey(User, verbose_name='Author', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Creation Date')
-    updated_at = models.DateTimeField(auto_now=True, verbose_name = 'Edition Date')
-    published_at =  models.DateTimeField(verbose_name='Published Date', default=now)
+    updated_at = models.DateTimeField(auto_now=True, verbose_name = 'Edition Date')    
 
     class Meta:
         verbose_name = "Post"
@@ -23,20 +24,3 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
-    
-    def formatted_content(self):
-        paragraphs = re.split(r'\n+', self.content.strip())  # Divide el contenido en párrafos
-        
-        if not paragraphs:  
-            return ""  # Si no hay contenido, retorna vacío
-        
-        first_two = "".join(f"<p>{p}</p>" for p in paragraphs[:3])  # Primeros 2 párrafos
-        blockquote = f"<blockquote><p>{self.relevant_text}</p></blockquote>" if self.relevant_text else ""
-        subtitle = f'<h3 style="text-align:left">{ self.subtitle }</h3>' 
-        remaining = "".join(f"<p>{p}</p>" for p in paragraphs[2:])  # Resto de los párrafos
-
-        return first_two + blockquote + subtitle + remaining  # Concatenar todo
-    
-    def count_paragraphs(self):
-        return len(re.split(r'\n+', self.content.strip()))
-    
