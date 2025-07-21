64b75da30f52169f749ae5712cd7f668b5d91b02
jnfmunoz <juanmunozgrau@gmail.com>
2025-07-21 02:50:45 -0400

    41

--- blog/models.py
+++ blog/models.py
@@ -9,9 +9,9 @@ class Post(models.Model):
     title = models.CharField(max_length=200, verbose_name='Title')
     subtitle = models.CharField(max_length=200, verbose_name='Subtitle')
     relevant_text = models.CharField(max_length=50, verbose_name='Relevant Text', null=True, blank=True)
-    introduction = models.TextField(verbose_name='Introduction')
-    body_text = models.TextField(verbose_name='Body Text')
-    conclusion = models.TextField(verbose_name='Conclusion')
+    introduction = models.TextField(verbose_name='Introduction', null=True, blank=True)
+    body_text = models.TextField(verbose_name='Body Text', null=True, blank=True)
+    conclusion = models.TextField(verbose_name='Conclusion', null=True, blank=True)
     image = models.ImageField(verbose_name='Image', upload_to='posts', null=False, blank=False)
     author = models.ForeignKey(User, verbose_name='Author', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Creation Date')
@@ -24,3 +24,26 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+
+    def formatted_content(self):
+        html_parts = []
+
+        if self.introduction:
+            intro_paragraphs = re.split(r'\n+', self.introduction.strip())
+            html_parts.append("".join(f"<p>{p}</p>" for p in intro_paragraphs))
+
+        if self.relevant_text:
+            html_parts.append(f'<blockquote><p>{self.relevant_text}</p></blockquote>')
+
+        if self.subtitle:
+            html_parts.append(f'<h3 style="text-align:left"></h3>')
+        
+        if self.body_text:
+            body_paragraphs = re.split(r'\n+', self.body_text.strip())
+            html_parts.append("".join(f"<p>{p}</p>" for p in body_paragraphs))
+
+        if self.conclusion:
+            conclusion_paragraphs = re.split(r'\n+', self.conclusion.strip())
+            html_parts.append("".join(f"<p>{p}</p>" for p in conclusion_paragraphs))
+
+        return "".join(html_parts)
