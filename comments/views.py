6c94def5cf93bf52c19fdc497d0a65bca465eda7
jnfmunoz <juanmunozgrau@gmail.com>
2025-08-27 23:50:11 -0400

    66

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from .models import Comment
+
+
 
 # Create your views here.
