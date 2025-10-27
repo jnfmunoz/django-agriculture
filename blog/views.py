3a4f090c98957aa66a4feeded8401c4cca3e8e3c
jnfmunoz <juanmunozgrau@gmail.com>
2025-10-27 16:00:23 -0300

    71

--- blog/views.py
+++ blog/views.py
@@ -42,6 +42,7 @@ class PostListView(ListView):
 #         context['comments'] = Comment.objects.filter(post=self.object).order_by('-created')
 #         return context
 
+
 class PostDetailView(DetailView):
     model = Post
     template_name = "blog/post_detail.html"
@@ -78,6 +79,47 @@ class PostDetailView(DetailView):
         context["comments"] = post.comments.all()
         context["comments_count"] = post.comments.count()     
         return context
+'''
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+    context_object_name = "post"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comments'] = Comment.objects.filter(post=self.object).order_by('-created')
+        context['form'] = CommentForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = self.object
+            comment.author = request.user
+            comment.save()
+
+            # Si es AJAX, devolvemos solo el comentario recién agregado
+            if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+                context = {'comment': comment}
+                return JsonResponse({
+                    'author': comment.author.get_full_name() or comment.author.username,
+                    'content': comment.content,
+                    'created': comment.created.strftime("%b %d, %Y"),
+                    'profile_photo_url': comment.author.profile.profile_photo.url
+                })
+            
+            # Si no es AJAX, redirigimos a la misma página
+            return redirect("blog:post_detail", pk=self.object.pk)
+
+        # Si el formulario no es válido, renderizamos la página con errores
+        context = self.get_context_data()
+        context['form'] = form
+        return self.render_to_response(context)
+
+'''
 
 class PostCreateView(CreateView):
 
